546141e40d5f0b53135dd5f3d5711184973bbf7f
taylorg <tegrubbs1@gmail.com>
2022-08-09 11:07:30 -0400

    updated install script. Adding build script for versioning

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-mkdir $HOME/.log
+INSTALL_DIR="/usr/bin"
+EXECUTABLE="log"
+CONFIG_DIR=".log"
+
 config='Username Moscow\nServer localhost:1241'
 
-config_path=$HOME/.log/config
+config_path=$HOME/$CONFIG_DIR/config
 
 # Don't overwrite old config
 if [ ! -f "$config_path" ]; then
+  mkdir $HOME/$CONFIG_DIR
   echo -e $config > $config_path
 fi
 
-sudo cp log /usr/bin/
-sudo chown root /usr/bin/log
-sudo chmod u+s /usr/bin/log
+# make executable suid binary
+sudo cp build/$EXECUTABLE $INSTALL_DIR
+sudo chown root $INSTALL_DIR/$EXECUTABLE
+sudo chmod u+s $INSTALL_DIR/$EXECUTABLE
 
-
-echo '### \nLog installation complete\n ###'
+echo -e '### Log installation complete ###'
--- make_root_suid.sh
+++ make_root_suid.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 cargo build
 
-cp target/debug/log log
-sudo chown root log
-sudo chmod u+s log
+EXEC_LOC=target/debug/log
 
-# sudo cp log /usr/bin/
-# sudo chown root /usr/bin/log
-# sudo chmod u+s /usr/bin/log
+sudo chown root $EXEC_LOC
+sudo chmod u+s $EXEC_LOC
 
 # ./log
-./log -c -in $HOME/Desktop/lammps/examples/crack
+$EXEC_LOC -c -in $HOME/Desktop/lammps/examples/crack
 # ./log mpirun -np 4 lmp -in $HOME/Desktop/lammps/examples/crack/in.crack
